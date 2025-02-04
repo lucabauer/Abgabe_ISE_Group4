a677e8f60bd7deb01aebc1eaf15ed74806088caa
Victor Gandsha <vic.gandsha@gmail.com>
2025-02-04 18:12:46 +0100

    Startseite Update

--- DataSets_Group4/Startseite.py
+++ DataSets_Group4/Startseite.py
@@ -1,17 +1,21 @@
 import streamlit as st
 
 def startseite():
-    st.title("Football Result Predictor")
-    st.write("Hier kann man Vorhersagen zu Spielergebnissen sehen , "
-        "die basierend auf vergangenen Spielen sind.")
+    st.title("⚽️ Football Result Predictor")
+    st.write("Willkommen zu unserem interaktiven Fußball-Dashboard! Hier kannst du nicht nur vergangene Länderspiele eines Teams einsehen, sondern auch die Gewinnwahrscheinlichkeit zwischen zwei Teams berechnen.")
+    st.write("✨ Funktionen:")
+    st.write("📅 Alle Spiele eines Teams anzeigen – Wähle ein Team aus und erhalte eine Übersicht über alle vergangenen Begegnungen.")
+    st.write("📊 Gewinnwahrscheinlichkeit berechnen – Vergleiche zwei Teams und erhalte eine Einschätzung darüber, welches Team die besseren Chancen auf den Sieg hat.")
 
     if st.button("Start"):
         st.switch_page("calculator.py")
 
+    st.image("Stadion.jpg")
+
 def calculation():
     st.title("Rechner")
 
-def AllGamesOfCountry():
+def all_games_of_country():
     st.title("Alle Spiele eines Landes")
 
 pg = st.navigation([st.Page(startseite), st.Page("calculator.py"), st.Page("AllGamesOfCountry.py")])
